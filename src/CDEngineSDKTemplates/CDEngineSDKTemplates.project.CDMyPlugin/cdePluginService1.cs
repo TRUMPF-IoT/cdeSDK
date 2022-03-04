5def5e00db6298e337a40fe23a213087aa6e3961
Chris Muench <chris.muench@c-labs.com>
2022-03-04 16:46:45 -0700

    Template improvements (#26)
    * new cdeEnergy model used by several plugins
    
    * dep5 fixes
    
    * missing license
    
    * content file fixes for energy and webconnect
    
    * Default Sensor improvements; SenderBase UX Improvements
    
    * SDK Updates to support VS2022
    
    * Build: use latest msbuild (for vsix 2022)
    
    * Build: lock to windows-2022 temporarily (for VSIX)
    
    * VSIX projects: remove unused Microsoft.VisualStudio.CoreUtility reference
    
    * VSIX project: remove one more Microsoft.VisualStudio.CoreUtility reference
    
    * VSIX Template: AppHostWinForms to .Net 4.7.2
    
    * Sensor Template updates; Energy Model Updates
    
    * New cdeNMIHelper Library and EnergyModel
    
    * Tagged AutoConnect in SensorTemplate correctly
    
    * Template optimization using quick aliases
    
    * fix for energy contract
    
    * missing license info
    
    * more licenses
    
    * more lic
    
    * typos
    
    * nmihelper reference missing
    
    * cleanup and copyright to 2022
    
    Co-authored-by: Chris Muench <chris.muench@live.com>
    Co-authored-by: Markus Horstmann <markushorstmann@hotmail.com>

--- src/CDEngineSDKTemplates/CDEngineSDKTemplates.project.CDMyPlugin/cdePluginService1.cs
+++ src/CDEngineSDKTemplates/CDEngineSDKTemplates.project.CDMyPlugin/cdePluginService1.cs
@@ -2,53 +2,48 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
-
-using nsCDEngine.BaseClasses;
-using nsCDEngine.Engines;
-using nsCDEngine.Engines.NMIService;
-using nsCDEngine.Engines.ThingService;
-using nsCDEngine.ViewModels;
+using NMI = nsCDEngine.Engines.NMIService.TheNMIEngine;
 
 namespace $safeprojectname$
 {
     public class e$safeprojectname$DeviceTypes : TheDeviceTypeEnum
     {
         public const string cdeThingDeviceTypeA = "My Device Type A";
-    }
+}
 
-    [EngineAssetInfo(
-        FriendlyName = strFriendlyName,
-        Capabilities = new[] { eThingCaps.ConfigManagement, },
-        EngineID = "{$guid2$}",
-        IsService = true,
-        LongDescription = "This service...",
-        IconUrl = "toplogo-150.png", // TODO Add your own icon
-        Developer = "C-Labs", // TODO Add your own name and URL
-        DeveloperUrl = "http://www.c-labs.com",
-        ManifestFiles = new string[] { }
-    )]
-    class cdePluginService1 : ThePluginBase
+[EngineAssetInfo(
+    FriendlyName = strFriendlyName,
+    Capabilities = new[] { eThingCaps.ConfigManagement, },
+    EngineID = "{$guid2$}",
+    IsService = true,
+    LongDescription = "This service...",
+    IconUrl = "toplogo-150.png", // TODO Add your own icon
+    Developer = "C-Labs", // TODO Add your own name and URL
+    DeveloperUrl = "http://www.c-labs.com",
+    ManifestFiles = new string[] { }
+)]
+class cdePluginService1 : ThePluginBase
+{
+    // TODO: Set plugin friendly name for InitEngineAssets (optional)
+    public const String strFriendlyName = "My Hello World Service";
+
+    public override bool Init()
     {
-        // TODO: Set plugin friendly name for InitEngineAssets (optional)
-        public const String strFriendlyName = "My Hello World Service";              
-
-        public override bool Init()
+        if (!mIsInitCalled)
         {
-            if (!mIsInitCalled)
+            mIsInitCalled = true;
+            MyBaseThing.StatusLevel = 4;
+            SetMessage("Service has started", DateTimeOffset.Now);
+
+            MyBaseThing.RegisterEvent(eEngineEvents.IncomingMessage, HandleMessage);
+            MyBaseEngine.RegisterEvent(eEngineEvents.ThingDeleted, OnThingDeleted);
+
+            // If not lengthy initialized you can remove cdeRunasync and call this synchronously
+            TheCommonUtils.cdeRunAsync(MyBaseEngine.GetEngineName() + " Init Services", true, (o) =>
             {
-                mIsInitCalled = true;
-                MyBaseThing.StatusLevel = 4;
-                SetMessage("Service has started", DateTimeOffset.Now);
-
-                MyBaseThing.RegisterEvent(eEngineEvents.IncomingMessage, HandleMessage);
-                MyBaseEngine.RegisterEvent(eEngineEvents.ThingDeleted, OnThingDeleted);
-
-                // If not lengthy initialized you can remove cdeRunasync and call this synchronously
-                TheCommonUtils.cdeRunAsync(MyBaseEngine.GetEngineName() + " Init Services", true, (o) =>
-                {
                     // Perform any long-running initialization (i.e. network access, file access) here that must finish before other plug-ins or the C-DEngine can use the plug-in
                     InitServices();
 
@@ -57,103 +52,103 @@ namespace $safeprojectname$
                     FireEvent(eThingEvents.Initialized, this, true, true); // Notify the C-DEngine and other plug-ins that the thing is initialized
                     MyBaseEngine.ProcessInitialized(); //Set the status of the Base Engine according to the status of the Things it manages
                 });
-            }
-            return false;
         }
+        return false;
+    }
 
-        // User-interface defintion
-        TheDashboardInfo mMyDashboard;
+    // User-interface defintion
+    TheDashboardInfo mMyDashboard;
 
-        public override bool CreateUX()
+    public override bool CreateUX()
+    {
+        if (!mIsUXInitCalled)
         {
-            if (!mIsUXInitCalled)
-            {
-                mIsUXInitCalled = true;
+            mIsUXInitCalled = true;
 
-                mMyDashboard = TheNMIEngine.AddDashboard(MyBaseThing, new TheDashboardInfo(MyBaseEngine, "My Demo Plugin Screen with Things"));
+            mMyDashboard = NMI.AddDashboard(MyBaseThing, new TheDashboardInfo(MyBaseEngine, "My Demo Plugin Screen with Things"));
 
-                var tFlds=TheNMIEngine.CreateEngineForms(MyBaseThing, TheThing.GetSafeThingGuid(MyBaseThing, "MYNAME"), "List of $safeitemrootname$", null, 20, 0x0F, 0xF0, TheNMIEngine.GetNodeForCategory(), "REFFRESHME", true, new e$safeprojectname$DeviceTypes(), e$safeprojectname$DeviceTypes.cdeThingDeviceTypeA);
-                TheFormInfo tForm = tFlds["Form"] as TheFormInfo;
-                tForm.AddButtonText = "Add new $safeitemrootname$";
+            var tFlds = NMI.CreateEngineForms(MyBaseThing, TheThing.GetSafeThingGuid(MyBaseThing, "MYNAME"), "List of $safeitemrootname$", null, 20, 0x0F, 0xF0, NMI.GetNodeForCategory(), "REFFRESHME", true, new e$safeprojectname$DeviceTypes(), e$safeprojectname$DeviceTypes.cdeThingDeviceTypeA);
+            TheFormInfo tForm = tFlds["Form"] as TheFormInfo;
+            tForm.AddButtonText = "Add new $safeitemrootname$";
 
-                TheNMIEngine.RegisterEngine(MyBaseEngine); //Registers this engine and its resources with the C-DEngine
-                mIsUXInitialized = true;
-            }
-            return true;
+            NMI.RegisterEngine(MyBaseEngine); //Registers this engine and its resources with the C-DEngine
+            mIsUXInitialized = true;
         }
+        return true;
+    }
 
-        void InitServices()
+    void InitServices()
+    {
+        List<TheThing> tDevList = TheThingRegistry.GetThingsOfEngine(MyBaseThing.EngineName);
+        foreach (TheThing tDev in tDevList)
         {
-            List<TheThing> tDevList = TheThingRegistry.GetThingsOfEngine(MyBaseThing.EngineName); 
-            foreach (TheThing tDev in tDevList)
+            if (!tDev.HasLiveObject)
             {
-			    if (!tDev.HasLiveObject)
-				{
-                    // There is no .Net class instance associated with this TheThing: create and register it, so the C-DEngine can find it
-                    switch (tDev.DeviceType)
-                    {
-                        // If your class does not follow the naming convention CDMyPlugin1.<fieldname>, you may need to instantiate it explicitly like this:
-                        //case e$safeprojectname$DeviceTypes.cdeThingDeviceTypeA:
-                        //    TheThingRegistry.RegisterThing(new cdeThingDeviceTypeA(tDev, this));
-                        //    break;
-                        default:
-                            // Assume the e$safeprojectname$DeviceTypes field names match the class names: find the field that corresponds to the TheThing.DeviceType being requested
-                            var fields = typeof(e$safeprojectname$DeviceTypes).GetFields();
-                            foreach (FieldInfo deviceTypeField in fields)
+                // There is no .Net class instance associated with this TheThing: create and register it, so the C-DEngine can find it
+                switch (tDev.DeviceType)
+                {
+                    // If your class does not follow the naming convention CDMyPlugin1.<fieldname>, you may need to instantiate it explicitly like this:
+                    //case e$safeprojectname$DeviceTypes.cdeThingDeviceTypeA:
+                    //    TheThingRegistry.RegisterThing(new cdeThingDeviceTypeA(tDev, this));
+                    //    break;
+                    default:
+                        // Assume the e$safeprojectname$DeviceTypes field names match the class names: find the field that corresponds to the TheThing.DeviceType being requested
+                        var fields = typeof(e$safeprojectname$DeviceTypes).GetFields();
+                        foreach (FieldInfo deviceTypeField in fields)
+                        {
+                            var deviceTypeValue = deviceTypeField.GetValue(new e$safeprojectname$DeviceTypes()) as string;
+                            if (deviceTypeValue == null || deviceTypeField.FieldType.FullName != "System.String") continue;
+                            if (deviceTypeValue == tDev.DeviceType)
                             {
-                                var deviceTypeValue = deviceTypeField.GetValue(new e$safeprojectname$DeviceTypes()) as string;
-                                if (deviceTypeValue == null || deviceTypeField.FieldType.FullName != "System.String") continue;
-                                if (deviceTypeValue == tDev.DeviceType)
+                                // Found a matching field: create an instance of the class based on the field's name, and register it with the C-DEngine
+                                var deviceTypeClass = Type.GetType("$safeprojectname$." + deviceTypeField.Name);
+                                if (deviceTypeClass == null)
                                 {
-                                    // Found a matching field: create an instance of the class based on the field's name, and register it with the C-DEngine
-                                    var deviceTypeClass = Type.GetType("$safeprojectname$." + deviceTypeField.Name);
-                                    if (deviceTypeClass == null)
-                                    {
-                                        deviceTypeClass = Type.GetType("$safeprojectname$.ViewModel." + deviceTypeField.Name);
-                                    }
-                                    if (deviceTypeClass != null)
-                                    {
-                                        TheThingRegistry.RegisterThing(Activator.CreateInstance(deviceTypeClass, tDev, this.GetBaseEngine()) as ICDEThing);
-                                    }
-                                    break;
+                                    deviceTypeClass = Type.GetType("$safeprojectname$.ViewModel." + deviceTypeField.Name);
                                 }
+                                if (deviceTypeClass != null)
+                                {
+                                    TheThingRegistry.RegisterThing(Activator.CreateInstance(deviceTypeClass, tDev, this.GetBaseEngine()) as ICDEThing);
+                                }
+                                break;
                             }
-                            break;
-                    }
+                        }
+                        break;
                 }
             }
-            MyBaseEngine.SetStatusLevel(-1); //Calculates the current statuslevel of the service/engine
         }
+        MyBaseEngine.SetStatusLevel(-1); //Calculates the current statuslevel of the service/engine
+    }
 
-        void OnThingDeleted(ICDEThing pEngine, object pDeletedThing) // CODE REVIEW: Is this really still needed?
+    void OnThingDeleted(ICDEThing pEngine, object pDeletedThing) // CODE REVIEW: Is this really still needed?
+    {
+        if (pDeletedThing is ICDEThing)
         {
-            if (pDeletedThing is ICDEThing)
-            {
-                //TODO: Stop Resources, Thread etc associated with this Thing
-                ((ICDEThing)pDeletedThing).FireEvent(eEngineEvents.ShutdownEvent, pEngine, null, false);
-            }
+            //TODO: Stop Resources, Thread etc associated with this Thing
+            ((ICDEThing)pDeletedThing).FireEvent(eEngineEvents.ShutdownEvent, pEngine, null, false);
         }
+    }
 
-//TODO: Step 4: Write your Business Logic
+    //TODO: Step 4: Write your Business Logic
 
-#region Message Handling
-public override void HandleMessage(ICDEThing sender, object pIncoming)
+    #region Message Handling
+    public override void HandleMessage(ICDEThing sender, object pIncoming)
+    {
+        TheProcessMessage pMsg = pIncoming as TheProcessMessage;
+        if (pMsg == null) return;
+
+        string[] cmd = pMsg.Message.TXT.Split(':');
+        switch (cmd[0])
         {
-            TheProcessMessage pMsg = pIncoming as TheProcessMessage;
-            if (pMsg == null) return;
-
-            string[] cmd = pMsg.Message.TXT.Split(':');
-            switch (cmd[0])
-            {
-                case "CDE_INITIALIZED":
-                    MyBaseEngine.SetInitialized(pMsg.Message);      //Sets the Service to "Ready". ProcessInitialized() internally contains a call to this Handler and allows for checks right before SetInitialized() is called. 
-            break;
-                case "REFFRESHME":
-                    InitServices();
-                    mMyDashboard.Reload(pMsg, false);
-                    break;
-            }
+            case "CDE_INITIALIZED":
+                MyBaseEngine.SetInitialized(pMsg.Message);      //Sets the Service to "Ready". ProcessInitialized() internally contains a call to this Handler and allows for checks right before SetInitialized() is called. 
+                break;
+            case "REFFRESHME":
+                InitServices();
+                mMyDashboard.Reload(pMsg, false);
+                break;
         }
-#endregion
-	}
+    }
+    #endregion
+}
 }
