d77e81cc3587a93269ab15c87eb6935b1c4569fc
Chris Muench <chris.muench@c-labs.com>
2022-11-19 12:18:53 -0800

    New Energy Model and DefaultSensor update (#33)
    * new cdeEnergy model used by several plugins
    
    * dep5 fixes
    
    * missing license
    
    * content file fixes for energy and webconnect
    
    * Default Sensor improvements; SenderBase UX Improvements
    
    * SDK Updates to support VS2022
    
    * Build: use latest msbuild (for vsix 2022)
    
    * Build: lock to windows-2022 temporarily (for VSIX)
    
    * VSIX projects: remove unused Microsoft.VisualStudio.CoreUtility reference
    
    * VSIX project: remove one more Microsoft.VisualStudio.CoreUtility reference
    
    * VSIX Template: AppHostWinForms to .Net 4.7.2
    
    * Sensor Template updates; Energy Model Updates
    
    * New cdeNMIHelper Library and EnergyModel
    
    * Tagged AutoConnect in SensorTemplate correctly
    
    * Template optimization using quick aliases
    
    * fix for energy contract
    
    * missing license info
    
    * more licenses
    
    * more lic
    
    * typos
    
    * nmihelper reference missing
    
    * cleanup and copyright to 2022
    
    * Sender Base: match subscriptions with ThingReference.ID correctly
    
    * fix for senderthing NMI
    
    * Energy Model Update
    
    * drop support for net35, net40 and move net45 to net48
    
    * packetinstaller to net48
    
    * remove of old ifdefs
    
    * Fixes for CDE Templates
    
    * Default sensor updates with new Energy Model
    
    * Cleanup
    
    Co-authored-by: Chris Muench <chris.muench@live.com>
    Co-authored-by: Markus Horstmann <markushorstmann@hotmail.com>

--- src/MessageContracts/034 - EnergyModel/cdeEnergyModel/cdeEnergyModel.cs
+++ src/MessageContracts/034 - EnergyModel/cdeEnergyModel/cdeEnergyModel.cs
@@ -9,6 +9,10 @@ using System;
 using CU = nsCDEngine.BaseClasses.TheCommonUtils;
 using TCC = nsCDEngine.Communication.TheCommCore;
 using TT = nsCDEngine.Engines.ThingService.TheThing;
+using NMI = nsCDEngine.Engines.NMIService.TheNMIEngine;
+using System.Collections.Generic;
+using nsCDEngine.ViewModels;
+using cdeEnergyBase;
 
 namespace cdeEnergyBase
 {
@@ -29,14 +33,49 @@ namespace cdeEnergyBase
         public const string EnergyStorageUpdate = "EnergyStorageUpdate";
         public const string EnergyProducerUpdate = "EnergyStoragProducerUpdate";
         public const string EnergyConsumerUpdate = "EnergyConsumerUpdate";
+        public const string EnergySensorUpdate = "EnergySensorUpdate";
+        public const string EnergyTransformerUpdate = "EnergyTransformerUpdate";
+        public const string EnergyDistributionUpdate = "EnergyDistributionUpdate";
         public const string EnergyBreakerUpdate = "EnergyBreakerUpdate";
         public const string EnergyPanelUpdate = "EnergyPanelUpdate";
         public const string EnergyTankUpdate = "EnergyTankUpdate";
+        public const string EnergySiteConsumptionUpdate = "EnergySiteConsumptionUpdate";
+        public const string EnergySitePowerOutageDetected = "EnergySitePowerOutageDetected";
+        public const string EnergySitePowerOutageResolved = "EnergySitePowerOutageResolved";
+        public const string EnergySiteOnBattery = "EnergySiteOnBattery";
+        public const string EnergySiteOnGrid = "EnergySiteOnGrid";
+    }
+
+    public class TheEnergyDeviceInfo : TheDataBase
+    {
+        public string DeviceType { get; set; }
+        public string MessageContract { get; set; }
+        public string OPCUADefinitionTypeId { get; set; }
+        public TheEnergyDeviceInfo()
+        {
+
+        }
+        public TheEnergyDeviceInfo(string pType, string pContract, string pUATypeId)
+        {
+            DeviceType = pType;
+            MessageContract = pContract;
+            OPCUADefinitionTypeId = pUATypeId;
+        }
     }
 
     [OPCUAType(UATypeNodeId = "nsu=http://c-labs.com/UA/EnergyDevices;i=1001")]
     public class TheEnergyBase : TheThingBase
     {
+        public static readonly List<TheEnergyDeviceInfo> EnergyDevices = new List<TheEnergyDeviceInfo>
+        {
+            new TheEnergyDeviceInfo ( "Energy Storage", eEnergyMessages.EnergyStorageUpdate, "nsu=http://c-labs.com/UA/EnergyDevices;i=1002" ) ,
+            new TheEnergyDeviceInfo ( "Energy Producer", eEnergyMessages.EnergyProducerUpdate, "nsu=http://c-labs.com/UA/EnergyDevices;i=1004"),
+            new TheEnergyDeviceInfo( "Energy Consumer", eEnergyMessages.EnergyConsumerUpdate , "nsu=http://c-labs.com/UA/EnergyDevices;i=1001"),
+            new TheEnergyDeviceInfo( "Energy Transformer", eEnergyMessages.EnergyTransformerUpdate , "nsu=http://c-labs.com/UA/EnergyDevices;i=1021"),
+            new TheEnergyDeviceInfo("Energy Sensor", eEnergyMessages.EnergySensorUpdate , "nsu=http://c-labs.com/UA/EnergyDevices;i=1013"),
+            new TheEnergyDeviceInfo("Energy Distributor", eEnergyMessages.EnergyDistributionUpdate , "nsu=http://c-labs.com/UA/EnergyDevices;i=1000")
+        };
+
         [ConfigProperty]
         public int PublishInterval
         {
@@ -80,6 +119,9 @@ namespace cdeEnergyBase
         {
             CFAP = 1; //1KG if not specified
             TheCDEngines.RegisterNewMiniRelay("EnergyMessages");
+            MyBaseThing.RegisterProperty(nameof(Watts));
+            MyBaseThing.RegisterProperty(nameof(Volts));
+            MyBaseThing.RegisterProperty(nameof(Ampere));
             return base.Init();
         }
 
@@ -87,11 +129,16 @@ namespace cdeEnergyBase
 
         public virtual void SendEnergyData(eEnergyThingCaps pSenderType, TheEnergyData LastEnergyData, bool Force = false)
         {
+            SendEnergyData(pSenderType, LastEnergyData, MyBaseThing.cdeMID.ToString(), Force);
+        }
+        public virtual void SendEnergyData(eEnergyThingCaps pSenderType, TheEnergyData LastEnergyData, string pID, bool Force = false)
+        {
             if (LastEnergyData == null || (!Force && (PublishInterval == 0 || DateTimeOffset.Now.Subtract(LastPublish).TotalSeconds < PublishInterval)))
                 return;
             LastPublish = DateTimeOffset.Now;
             LastEnergyData.Time = DateTime.Now;
-            LastEnergyData.StationID = MyBaseThing.cdeMID;
+            if (LastEnergyData.StationID==Guid.Empty) 
+                LastEnergyData.StationID = MyBaseThing.cdeMID;
             string tMessageTxt = eEnergyMessages.EnergyConsumerUpdate;
             switch (pSenderType)
             {
@@ -111,8 +158,9 @@ namespace cdeEnergyBase
                     tMessageTxt = eEnergyMessages.EnergyTankUpdate;
                     break;
             }
-            LastEnergyData.StationName = $"{tMessageTxt}: {MyBaseThing.FriendlyName}"; 
-            TSM msgEnergy2 = new TSM("EnergyMessages", $"{tMessageTxt}:{MyBaseThing.cdeMID}", CU.SerializeObjectToJSONString(LastEnergyData));
+            if (string.IsNullOrEmpty(LastEnergyData.StationName))
+                LastEnergyData.StationName = $"{tMessageTxt}: {MyBaseThing.FriendlyName}"; 
+            TSM msgEnergy2 = new TSM("EnergyMessages", $"{tMessageTxt}:{pID}", CU.SerializeObjectToJSONString(LastEnergyData));
             msgEnergy2.SetNoDuplicates(true);
             TCC.PublishCentral(msgEnergy2, true);
         }
